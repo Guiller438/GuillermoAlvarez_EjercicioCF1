94dc47d05e1f70d4b6d1965528a1798c57575c9c
Guiller438 <guilleomg26.9@gmail.com>
2023-10-11 08:37:23 -0500

    Se agrego la primera tabla

---
+++ Models/Burger.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GuillermoAlvarez_EjercicioCF1.Models
+{
+    public class Burger
+    {
+        public int ID{ get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+        public bool WithCheese { get; set; }
+
+        [Range(0.01, 9999.99)]
+        public decimal Price { get; set; }
+
+       
+    }
+}
